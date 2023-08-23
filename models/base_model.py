90027c4e8269e51d8ba62be1afec4d641b97226d
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-23 20:01:38 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,19 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False, unique=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiate a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -28,17 +35,17 @@ class BaseModel:
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Updates updated_at with the current time when the instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        """Converts instance into dictionary format"""
+        dictionary = dict(self.__dict__)
+        dictionary['__class__'] = type(self).__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        # Remove the SQLAlchemy-specific attribute if it exists
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
