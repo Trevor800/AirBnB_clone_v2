408e1de92b86c3f002af08028f49175abf225d7b
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-23 20:04:00 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'  # Table name for SQLAlchemy
+
+    name = Column(String(128), nullable=False)
+    
+    # For DBStorage: Establish a one-to-many relationship with City
+    # If the State object is deleted, all linked City objects are automatically deleted.
+    cities = relationship('City', backref='state', cascade='all, delete-orphan', passive_deletes=True)
+
+    # For FileStorage: Create a getter attribute to return City instances with matching state_id
+    @property
+    def cities(self):
+        from models import storage
+        all_cities = storage.all("City")
+        return [city for city in all_cities.values() if city.state_id == self.id]
