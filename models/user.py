3f53f112eab88beefbfbaaf3a5be0ba0757ac5ad
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-24 01:13:48 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,15 +1,30 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+class User(BaseModel):
+    """Represents a user for a MySQL database.
 
-
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-
+    Attributes:
+        email: The user's email address.
+        password: The user's password.
+        first_name: The user's first name.
+        last_name: The user's last name.
+        places: The User-Place relationship.
+        reviews: The User-Review relationship.
+    """
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
 
+    def __init__(self, email, password, first_name, last_name):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __setattr__(self, name, value):
+        """Encodes passwords using MD5 before setting an attribute."""
+        if name == "password":
+            value = value.encode("utf-8")
+            value = md5(value).hexdigest()
+        super().__setattr__(name, value)
