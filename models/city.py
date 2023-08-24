e7ff2847363ac757e2313095f864d92e9ed8ed5c
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-24 03:12:18 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
-class City(BaseModel):
+
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'  # Table name as requested
+    __tablename__ = 'cities'  # Table name for SQLAlchemy
 
-    state_id = Column(String(60), nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
-    # Add a one-to-many relationship to Place
+    # Establish the relationship with Place
     places = relationship("Place", backref="cities", cascade="all, delete-orphan")
