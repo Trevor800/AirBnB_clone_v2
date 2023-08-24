161f6deeec8a1ecf9c1b8e668925f5791bca80b1
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-24 03:28:20 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
@@ -14,6 +13,5 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    # Establish the relationship with Place
-    places = relationship("Place", backref="user",
-                          cascade="all, delete-orphan")
+    # Add a one-to-many relationship to Review
+    reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
