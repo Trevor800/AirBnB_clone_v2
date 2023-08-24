49e0346720ae9e4836e45de3e10804fff90e2617
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-24 03:03:32 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from models.city import City
+from models.user import User
 
 
-class Place(BaseModel, Base):
+class Place(BaseModel):
     """ A place to stay """
-    __tablename__ = 'places'
+    __tablename__ = 'places'  # Table name for SQLAlchemy
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    amenity_ids = []
+
+    # Relationships
+    city = relationship("City")
+    user = relationship("User")
