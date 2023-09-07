7558ab2ee5b56d587131822c5675d9527ada6e3c
Trevor800 <jeynator780@gmail.com>
2023-09-07 16:28:25 -0700

    all done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,30 @@
 #!/usr/bin/env bash
+# Update package lists to ensure we have the latest package information
+sudo apt-get -y update
 
-# Install Nginx if not already installed
-if ! dpkg -l | grep -q nginx; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+# Install Nginx web server
+sudo apt-get -y install nginx
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+# Create necessary directories for web_static deployment
+sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
 
-# Create a fake HTML file for testing
-echo "Hello, web_static test page!" | sudo tee /data/web_static/releases/test/index.html
+# Create a simple HTML file for testing purposes and store it in the appropriate location
+echo "Holberton School for the win!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
-# Create or recreate the symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Remove any existing symbolic link at /data/web_static/current
+sudo rm -rf /data/web_static/current
 
-# Give ownership of /data/ to ubuntu user and group recursively
+# Create a symbolic link to make /data/web_static/releases/test/ the current version
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+# Change ownership of the /data/ directory and its contents to the 'ubuntu' user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-config_block="location /hbnb_static/ { alias /data/web_static/current/; }"
+# Define an Nginx server configuration block for serving /hbnb_static/
+server="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 
-# Check if config_block already exists, if so, remove it first
-if sudo grep -q "$config_block" "$config_file"; then
-    sudo sed -i "/$config_block/d" "$config_file"
-fi
+# Insert the server configuration block into the Nginx default site configuration
+sudo sed -i "38i $server" /etc/nginx/sites-available/default
 
-# Add the new config_block
-sudo sed -i "/server {/a $config_block" "$config_file"
-
-# Restart Nginx to apply changes
+# Restart Nginx to apply the new configuration
 sudo service nginx restart
-
-# Exit successfully
-exit 0
