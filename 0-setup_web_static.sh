e78ea09ea03c42851029e243de365283b09e525f
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-07 20:04:26 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,12 @@
 #!/usr/bin/env bash
 # Install Nginx if not already installed
-if ! [ -x "$(command -v nginx)" ]; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
-
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create or update the symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership to the ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-config_text="location /hbnb_static {
-    alias /data/web_static/current/;
-}
-"
-sudo sed -i "/server_name _;/a $config_text" /etc/nginx/sites-available/default
-
-# Restart Nginx
-sudo service nginx restart
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+serve="\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
+sed -i "s/^\tserver_name .*;$/&\n$serve/" /etc/nginx/sites-available/default
+service nginx restart
