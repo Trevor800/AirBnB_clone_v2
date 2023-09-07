113a9f2b28ca3ccb4f6d458ccc02c955d3b47b22
Trevor800 <jeynator780@gmail.com>
2023-09-07 11:37:53 -0700

    new correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,39 +1,38 @@
 #!/usr/bin/env bash
+# Sets up a web server for deployment of web_static.
 
-# Check if Nginx is installed, and if not, install it
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create or recreate symbolic link
-ln -sf /data/web_static/releases/test /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Give ownership to ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
 
-# Update Nginx configuration
-config_file="/etc/nginx/sites-available/default"
-nginx_config="location /hbnb_static {
-    alias /data/web_static/current;
-}"
+    location /redirect_me {
+	return 301 http://cuberule.com/;
+    }
 
-# Check if the configuration already exists, if not, add it
-if ! grep -q "location /hbnb_static" "$config_file"; then
-    sudo sed -i "/server_name _;/ a $nginx_config" "$config_file"
-fi
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-# Restart Nginx
-sudo service nginx restart
+service nginx restart
