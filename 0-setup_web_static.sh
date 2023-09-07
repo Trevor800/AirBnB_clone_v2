eed4696b3cb93633d1e8f37ef13b093c60691599
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-07 20:31:26 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,29 @@
 #!/usr/bin/env bash
-# Check if Nginx is installed, and if not, install it
-if ! command -v nginx &>/dev/null; then
-  apt-get update
-  apt-get install nginx -y
+# Update package lists and install Nginx
+sudo apt-get update
+sudo apt-get install -y nginx
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create a fake HTML file
+echo "Welcome to AirBnB" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+# Create or recreate symbolic link
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+# Give ownership to the ubuntu user and group recursively
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+nginx_config="location /hbnb_static {
+    alias /data/web_static/current;
+}"
+
+# Check if the configuration already exists, if not, add it
+if ! sudo grep -q "location /hbnb_static" /etc/nginx/sites-available/default; then
+    echo "$nginx_config" | sudo tee -a /etc/nginx/sites-available/default > /dev/null
 fi
 
-# Create the /data/web_static/releases/test directory
-mkdir -p /data/web_static/releases/test
-
-# Create the /data/web_static/shared directory
-mkdir -p /data/web_static/shared
-
-# Create the /data/web_static/releases/test/index.html file
-echo "Holberton School" > /data/web_static/releases/test/index.html
-
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
-ln -sf /data/web_static/releases/test /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user AND group
-chown -R ubuntu:ubuntu /data
-
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-# (ex: https://mydomainname.tech/hbnb_static). Don't forget to restart Nginx after updating the configuration:
-
-# Get the current Nginx configuration
-nginx_config=$(cat /etc/nginx/sites-available/default)
-
-# Add the following line to the Nginx configuration
-echo "server {
-  listen 80;
-  server_name localhost;
-
-  location /hbnb_static {
-    alias /data/web_static/current;
-  }
-}
-" >> $nginx_config
-
-# Update Nginx
-nginx -s reload
-
-# The script should always exit successfully
-exit 0
+# Restart Nginx
+sudo service nginx restart
