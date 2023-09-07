1c35a61232855e301d149f645614ba3f0b7670cc
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-07 20:14:11 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,38 @@
 #!/usr/bin/env bash
 # Install Nginx if not already installed
-apt-get update
-apt-get install -y nginx
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+
+# Create necessary folders
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+# Create or recreate symbolic link
 ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-serve="\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
-sed -i "s/^\tserver_name .*;$/&\n$serve/" /etc/nginx/sites-available/default
-service nginx restart
+
+# Give ownership to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config_file="/etc/nginx/sites-available/default"
+nginx_config="location /hbnb_static {
+    alias /data/web_static/current;
+}"
+
+# Check if the configuration already exists, if not, add it
+if ! grep -q "location /hbnb_static" "$config_file"; then
+    sudo sed -i "/server_name _;/ a $nginx_config" "$config_file"
+fi
+
+# Restart Nginx
+sudo service nginx restart
